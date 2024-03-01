52a0584136541514b4ce4676988293925e56ba63
claudetemachado <claudetemachado692@gmail.com>
2024-03-01 09:54:35 -0300

    estilização

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const axios =  require('axios');
 const path = require('path');
@@ -11,6 +12,7 @@ app.listen(3000);
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 const traducaoClima = {
     "few clouds": "Poucas nuvens",
@@ -32,7 +34,8 @@ app.get('/climatempo/:cidade', async(req,res) => {
             
             const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description;
             const weatherData = {
-                Temperatura: response.data.main.temp,
+                Local: response.data.name,
+                Temperatura: response.data.main,
                 Umidade: response.data.main.humidity,
                 VelocidadeDoVento: response.data.wind.speed,
                 Clima: clima
